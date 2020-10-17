a08c316e8aecab96235a675208787f82b7910319
t-li <tli_march@hotmail.com>
2020-10-16 18:57:18 -0600

    made data an argument for generate_predictions.sh

--- scripts/generate_predictions.sh
+++ scripts/generate_predictions.sh
@@ -7,6 +7,7 @@ gpuid=${gpuid:-0}
 if [ "$1" == "-h" ]; then
   echo "Generate predictions using all models and all datasets we have."
   echo "   --gpuid       The GPU device index to use, default to 0"
+  echo "   --d           A list of dataset types, separated by comma, must be in {gender, country, religion, ethnicity}"
   echo "   -h           Print the help message and exit"
   exit 0
 fi
@@ -23,28 +24,27 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-DATA=gender,country,ethnicity,religion
 # mased LMs
-./generate_lm_predictions.sh --m roberta-base --m_name robertabase --d $DATA --gpuid $gpuid
-./generate_lm_predictions.sh --m roberta-large --m_name robertalarge --d $DATA --gpuid $gpuid
-./generate_lm_predictions.sh --m distilbert-base-uncased --m_name distilbert --d $DATA --gpuid $gpuid
-./generate_lm_predictions.sh --m bert-base-uncased --m_name bertbase --d $DATA --gpuid $gpuid
-./generate_lm_predictions.sh --m bert-large-uncased-whole-word-masking --m_name bertlarge --d $DATA --gpuid $gpuid
+./generate_lm_predictions.sh --m roberta-base --m_name robertabase --d $d --gpuid $gpuid
+./generate_lm_predictions.sh --m roberta-large --m_name robertalarge --d $d --gpuid $gpuid
+./generate_lm_predictions.sh --m distilbert-base-uncased --m_name distilbert --d $d --gpuid $gpuid
+./generate_lm_predictions.sh --m bert-base-uncased --m_name bertbase --d $d --gpuid $gpuid
+./generate_lm_predictions.sh --m bert-large-uncased-whole-word-masking --m_name bertlarge --d $d --gpuid $gpuid
 
 # SQuAD models
 #   for roberta family, we use the ones trained on our own
-./generate_qa_predictions.sh --m ./models/squad_seqtok --m_name robertabase --t roberta-base --d $DATA --gpuid $gpuid
-./generate_qa_predictions.sh --m ./models/squad_seqtok_robertalarge --m_name robertalarge --t roberta-large --d $DATA --gpuid $gpuid
+./generate_qa_predictions.sh --m ./models/squad_seqtok --m_name robertabase --t roberta-base --d $d --gpuid $gpuid
+./generate_qa_predictions.sh --m ./models/squad_seqtok_robertalarge --m_name robertalarge --t roberta-large --d $d --gpuid $gpuid
 #   for bert base and dilstilbert, use the ones trained on our own with HF's internfaces
-./generate_qa_predictions_hf.sh --m ./models/bert-base-uncased-squad --m_name bertbase --d $DATA --gpuid $gpuid
-./generate_qa_predictions_hf.sh --m ./models/distilbert-base-uncased-squad --m_name distilbert --d $DATA --gpuid $gpuid
+./generate_qa_predictions_hf.sh --m ./models/bert-base-uncased-squad --m_name bertbase --d $d --gpuid $gpuid
+./generate_qa_predictions_hf.sh --m ./models/distilbert-base-uncased-squad --m_name distilbert --d $d --gpuid $gpuid
 #   for bertlarge, we use the officially released model from HF
-./generate_qa_predictions_hf.sh --m bert-large-uncased-whole-word-masking-finetuned-squad --m_name bertlarge --d $DATA --gpuid $gpuid
+./generate_qa_predictions_hf.sh --m bert-large-uncased-whole-word-masking-finetuned-squad --m_name bertlarge --d $d --gpuid $gpuid
 
 # NewsQA models
 #   here we use models trained on our own
 ./generate_qa_predictions.sh --m ./models/newsqa_seqtok --m_name newsqa_robertabase --t roberta-base --d  --gpuid $gpuid
-./generate_qa_predictions.sh --m ./models/newsqa_seqtok_robertalarge --m_name newsqa_robertalarge --t roberta-large --d $DATA --gpuid $gpuid
-./generate_qa_predictions.sh --m ./models/newsqa_seqtok_distilbert --m_name newsqa_distilbert --t distilbert-base-uncased --d $DATA --gpuid $gpuid
-./generate_qa_predictions.sh --m ./models/newsqa_seqtok_bertbase --m_name newsqa_bertbase --t bert-base-uncased --d $DATA --gpuid $gpuid
-./generate_qa_predictions.sh --m ./models/newsqa_seqtok_bertlarge --m_name newsqa_bertlarge --t bert-large-uncased-whole-word-masking --d $DATA --gpuid $gpuid
+./generate_qa_predictions.sh --m ./models/newsqa_seqtok_robertalarge --m_name newsqa_robertalarge --t roberta-large --d $d --gpuid $gpuid
+./generate_qa_predictions.sh --m ./models/newsqa_seqtok_distilbert --m_name newsqa_distilbert --t distilbert-base-uncased --d $d --gpuid $gpuid
+./generate_qa_predictions.sh --m ./models/newsqa_seqtok_bertbase --m_name newsqa_bertbase --t bert-base-uncased --d $d --gpuid $gpuid
+./generate_qa_predictions.sh --m ./models/newsqa_seqtok_bertlarge --m_name newsqa_bertlarge --t bert-large-uncased-whole-word-masking --d $d --gpuid $gpuid
